259f26eb888394995b07ca3a371721a8515f717b
Adkhambek <114991635+khusinboev@users.noreply.github.com>
2025-07-06 18:39:47 +0500

    Update messages.py

--- src/handlers/admins/messages.py
+++ src/handlers/admins/messages.py
@@ -31,7 +31,7 @@ markup = ReplyKeyboardMarkup(
 
 
 # === ADMIN PANEL === #
-@msg_router.message(F.text == "✍Xabarlar", F.chat.type == ChatType.PRIVATE, F.from_user.id.in_(ADMIN_ID))
+@msg_router.message(F.text == "✍Xabarlar", F.chat.type == ChatType.PRIVATE, AdminFilter(static_admins=ADMIN_ID))
 async def panel_handler(message: Message) -> None:
     await message.answer("Xabarlar bo'limi!", reply_markup=await AdminPanel.admin_msg())
 
