7443f764f60dd9d8a257b139ef88fc92122af20f
Windows 10 <adhambekhusinboyev@gmail.com>
2025-07-09 09:14:20 +0500

    first

--- src/keyboards/buttons.py
+++ src/keyboards/buttons.py
@@ -58,22 +58,22 @@ class AdminPanel:
 
     @staticmethod
     async def admin_msg():
-        admin_channel=ReplyKeyboardMarkup(
-                    keyboard=[
-                        [
-                            KeyboardButton(text="📨Forward xabar yuborish"),
-                            KeyboardButton(text="📬Oddiy xabar yuborish"),
-                        ],
-                        [
-                            KeyboardButton(text="🧪Sinov: Copy yuborish"),
-                            KeyboardButton(text="🧪Sinov: Forward yuborish")
-], 
-                        [
-                            KeyboardButton(text="🔙Orqaga qaytish"),
-                        ]
-                    ],
-                    resize_keyboard=True,
-                )
+        admin_channel = ReplyKeyboardMarkup(
+            keyboard=[
+                [
+                    KeyboardButton(text="📨Forward xabar yuborish"),
+                    KeyboardButton(text="📬Oddiy xabar yuborish"),
+                ],
+                [
+                    KeyboardButton(text="🧪Sinov: Copy yuborish"),
+                    KeyboardButton(text="🧪Sinov: Forward yuborish")
+                ],
+                [
+                    KeyboardButton(text="🔙Orqaga qaytish"),
+                ]
+            ],
+            resize_keyboard=True,
+        )
         return admin_channel
 
 
